7da6e0a054afff52a0b9f5b01adece54fab51edf
Phuc Le <phuclenguyenhoang28@gmail.com>
2026-01-31 11:31:50 +0700

    feat: db context

---
+++ src/main/java/Utils/DBContext.java
@@ -0,0 +1,25 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+    public Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=Glitzy;"
+                    + "user=sa;"
+                    + "password=280205;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=true;";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Database connection failed: " + ex);
+        }
+    }
+}
