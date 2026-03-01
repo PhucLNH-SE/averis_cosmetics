34df375ca3f4b999b2691ad772c83591605459cb
QuangTT <tthanhquang.ce190271@gmail.com>
2026-03-01 19:29:36 +0700

    QuangTT - Manage Catagory

--- src/main/java/Controllers/admin/CatagoryController.java
+++ src/main/java/Controllers/admin/CatagoryController.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AdminCategoryController extends HttpServlet {
+public class CatagoryController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
